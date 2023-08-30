f35a89b42c46f8e50df82e20851984d6b206e368
user <user@DESKTOP-LI69LEU>
2023-08-30 21:47:53 +0300

    DataBindingDONE

---
+++ DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Windows;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+        private string boundText;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string BoundText
+        {
+            get { return boundText; }
+            set 
+            {
+                boundText = value;
+                OnPropertyChanged();
+            }
+        }
+        private void OnPropertyChanged([CallerMemberName] string propertyName=null)
+        {
+            PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void btnSet_Click(object sender, RoutedEventArgs e)
+        {
+            BoundText = "Set from code";
+        }
+    }
+}
